c4bda507adbd4e6a8e6dff338f4e22f4822b6a64
Jakub Mamala <g00408184@atu.ie>
2022-11-21 09:53:51 +0000

    file writer

--- src/main/java/ie/atu/creatFile.java
+++ src/main/java/ie/atu/creatFile.java
@@ -1,15 +1,28 @@
 package ie.atu;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class creatFile {
     public static void main(String[] args) {
 
-        {
+
             File myFile = new File("myfile.txt");
             System.out.println("My file is located at" + myFile.getAbsolutePath());
+            try {
+                FileWriter myWriter = new FileWriter(myFile);
+                myWriter.write("this is my first line");
+                myWriter.close();
+
+            }catch (IOException e)
+            {
+                e.printStackTrace();
+            }
 
 
-        }
+
+
+
     }
 }
