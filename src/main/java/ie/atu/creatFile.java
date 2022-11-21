487d8e5e2a5817e81e9e0292ed944982a0788a7e
Jakub Mamala <g00408184@atu.ie>
2022-11-21 10:51:33 +0000

    last step

--- src/main/java/ie/atu/creatFile.java
+++ src/main/java/ie/atu/creatFile.java
@@ -1,15 +1,38 @@
 package ie.atu;
 
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class creatFile {
     public static void main(String[] args) {
-
-        {
-            File myFile = new File("myfile.txt");
+            System.out.println("Enter file name");
+            Scanner input = new Scanner(System.in);
+            String name = input.nextLine();
+            File myFile = new File(name);
             System.out.println("My file is located at" + myFile.getAbsolutePath());
 
+            try {
+                PrintWriter myWriter = new PrintWriter(new FileWriter("myfile.txt", true));
+                myWriter.println("this is my first line");
+                myWriter.close();
 
-        }
+
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Scanner inputFile = null;
+            try{
+                inputFile = new Scanner(new File("myfile.txt"));
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            while (inputFile.hasNext())
+            {
+                String str = inputFile.nextLine();
+                System.out.println(str);
+            }
+
     }
 }
